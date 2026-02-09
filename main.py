7984724290502acca3657ed248ff0f9c4e64d288
Anshika Gupta <anshika.akg19@gmail.com>
2026-02-09 08:55:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #Calculator Program 
+import addition as a
+import subtraction as s
+import multiplication as m
+print("Addition = ", a.add(5,6))
+print("Subtraction = ", s.sub(20,4))
+print("Multiplication = ", m.mul(4,5))
