6e78d6de94e6f47de166bd7343a748513c1be60a
Anshika Gupta <anshika.akg19@gmail.com>
2026-02-09 08:55:41 +0530

    Add function calls for addition, subtraction, and multiplication

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import addition as a
 import subtraction as s
 import multiplication as m
+
+#calling add, sub and mul function
 print("Addition = ", a.add(5,6))
 print("Subtraction = ", s.sub(20,4))
 print("Multiplication = ", m.mul(4,5))
